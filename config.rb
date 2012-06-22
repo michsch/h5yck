f392289cd62be7573360b4e32922ff072bb00044
Michael Schulze <m.schulze@elsigno.de>
2012-06-22 21:49:14 +0200

    new structure without assets folder

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/css/sass"
-images_dir = "assets/images"
-javascripts_dir = "assets/js"
+css_dir = "css"
+sass_dir = "css/sass"
+images_dir = "images"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
